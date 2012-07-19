fcd4cb876043b76b4cc1c7fcba13783ea258ea5e
3sidedcoin <tejas@3sided.co.in>
2012-07-19 15:55:53 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html> 
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ --> 
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <title></title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+ 	<link rel="stylesheet" href="assets/css/style.css" />
+	<script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+		
+		
+		});
+	</script>	
+</head>
+<body>
+	Index.php
+</body>
+</html>	
